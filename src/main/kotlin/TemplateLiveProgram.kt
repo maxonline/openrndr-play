60f6e66e2f9177ceb63f17cb9fea4841cb3117b7
Maximilian Skog <maximilian.skog@svt.se>
2023-03-03 15:44:49 +0100

    wip

--- src/main/kotlin/TemplateLiveProgram.kt
+++ src/main/kotlin/TemplateLiveProgram.kt
@@ -1,6 +1,18 @@
+import org.openrndr.KEY_ESCAPE
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
+import org.openrndr.draw.ColorBufferShadow
+import org.openrndr.draw.Drawer
+import org.openrndr.draw.loadImage
 import org.openrndr.extra.olive.oliveProgram
+import org.openrndr.ffmpeg.ScreenRecorder
+import org.openrndr.math.Vector2
+import org.openrndr.shape.Circle
+import org.openrndr.shape.Rectangle
+import org.openrndr.shape.ShapeNode
+import org.openrndr.shape.map
+import org.openrndr.svg.loadSVG
+import kotlin.math.roundToInt
 
 /**
  *  This is a template for a live program.
@@ -9,14 +21,82 @@ import org.openrndr.extra.olive.oliveProgram
  *  oliveProgram {} can be changed while the program is running.
  */
 
+data class Hej(val va: String)
+
+val windowRect = Rectangle(0.0, 0.0, 768.0, 576.0)
+val balls = mutableListOf<Ball>()
+val composition = loadSVG("data/images/svtplay.svg")
+
+
+
 fun main() = application {
-    configure {
-        width = 800
-        height = 800
-    }
-    oliveProgram {
-        extend {
-            drawer.clear(ColorRGBa.PINK)
+    for (x in 30..(windowRect.width.toInt()) - 30 step 60) {
+        for (y in 30..(windowRect.height.toInt()) - 30 step 60) {
+            balls.add(
+                Ball(
+                    Vector2(x.toDouble(), y.toDouble()),
+                    Vector2(Math.random(), Math.random()).normalized * 2.0
+                )
+            )
         }
+    }
+
+    configure {
+        width = windowRect.width.toInt()
+        height = windowRect.height.toInt()
+    }
+
+    oliveProgram {
+        val testbild = loadImage("data/images/pm5544.png")
+        val sh = testbild.shadow.apply { download() }
+        println(testbild.shadow[100, 100])
+        composition.root.map {
+            if (it is ShapeNode) {
+                it.copy().bounds
+            } else {
+                it
+            }
+        }
+
+        val recorder = ScreenRecorder().apply {
+            outputToVideo = false
+        }
+        extend(recorder)
+        extend {
+            drawer.clear(ColorRGBa.WHITE)
+            drawer.image(testbild)
+            balls.forEach { it.update(windowRect, drawer, sh) }
+            drawer.composition(composition)
+        }
+        keyboard.keyDown.listen {
+            when {
+                it.key == KEY_ESCAPE -> program.application.exit()
+                it.name == "v" -> {
+                    recorder.outputToVideo = !recorder.outputToVideo
+                    println(if (recorder.outputToVideo) "Recording" else "Paused")
+                }
+            }
+        }
+    }
+}
+
+class Ball(var position: Vector2, var velocity: Vector2) {
+    val radius = 16.0
+    fun update(windowRect: Rectangle, drawer: Drawer, sh: ColorBufferShadow) {
+        if (position.x < radius || position.x > windowRect.width - radius) {
+            velocity = velocity.copy(x = -velocity.x)
+        }
+        if (position.y < radius || position.y > windowRect.height - radius) {
+            velocity = velocity.copy(y = -velocity.y)
+        }
+        position += velocity
+        val colorFromTestbild = if (windowRect.contains(this.position)) {
+            sh[(this.position.x).toInt(), (this.position.y).toInt()]
+        } else {
+            ColorRGBa.BLACK
+        }
+        drawer.fill = colorFromTestbild
+        drawer.stroke = null
+        drawer.circle(position, radius)
     }
 }
