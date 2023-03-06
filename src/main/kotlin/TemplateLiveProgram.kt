b42d67198249d6bc31d10ebf55e2dd8fb4bfda24
linnhallstrom <linn.hallstrom@svt.se>
2023-03-06 10:08:28 +0100

    cleanup

--- src/main/kotlin/TemplateLiveProgram.kt
+++ src/main/kotlin/TemplateLiveProgram.kt
@@ -4,14 +4,10 @@ import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.ColorBufferShadow
 import org.openrndr.draw.Drawer
 import org.openrndr.draw.loadImage
-import org.openrndr.draw.shadeStyle
 import org.openrndr.extra.olive.oliveProgram
 import org.openrndr.ffmpeg.ScreenRecorder
-import org.openrndr.math.Matrix44
 import org.openrndr.math.Vector2
 import org.openrndr.shape.*
-import org.openrndr.svg.loadSVG
-import kotlin.math.roundToInt
 
 /**
  *  This is a template for a live program.
@@ -45,32 +41,15 @@ fun main() = application {
 
     oliveProgram {
         val testbild = loadImage("data/images/pm5544.png")
-        val composition = loadSVG("data/images/Primula_veris_floral_diagram.svg")
         val sh = testbild.shadow.apply { download() }
-
-        composition.root.map {
-           // it.findShapes().forEach { println("${it.id} _____ ${it.fill}  ____ ") }
-            it.strokeWeight = 2.0
-            if (it is ShapeNode) {
-                it.copy(shape = it.shape.apply { it.fill = ColorRGBa.TRANSPARENT })
-            } else {
-                it
-            }
-        }
-
-        //composition.findGroup("g73")?.fill = ColorRGBa(255.0,216.0,0.0)
-
-
         val recorder = ScreenRecorder().apply {
             outputToVideo = false
         }
         extend(recorder)
         extend {
             drawer.clear(ColorRGBa.WHITE)
-            //   drawer.translate(width / 8.9, height / 19.0)
             drawer.image(testbild)
             balls.forEach { it.update(windowRect, drawer, sh) }
-            drawer.composition(composition)
         }
         keyboard.keyDown.listen {
             when {
