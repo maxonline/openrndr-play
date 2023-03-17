2b43c15eaf2789415c2d897a8402f56eb222dbcc
Maximilian Skog <maximilian.skog@svt.se>
2023-03-17 11:41:30 +0100

    wip

--- src/main/kotlin/TemplateLiveProgram.kt
+++ src/main/kotlin/TemplateLiveProgram.kt
@@ -1,26 +1,21 @@
 import org.openrndr.*
 import org.openrndr.color.ColorRGBa
-import org.openrndr.draw.ColorBufferShadow
-import org.openrndr.draw.Drawer
-import org.openrndr.draw.loadImage
 import org.openrndr.extra.olive.oliveProgram
-import org.openrndr.ffmpeg.ScreenRecorder
 import org.openrndr.math.Vector2
-import org.openrndr.shape.*
-enum class ArrowKey {
-    DOWN,
-    UP,
-    LEFT,
-    RIGHT
-}
+import org.openrndr.shape.Rectangle
+
+enum class ArrowKey { DOWN, UP, LEFT, RIGHT }
 
 var arrow: ArrowKey? = null
 
 val windowRect = Rectangle(0.0, 0.0, 1000.0, 800.0)
 
-var playerPosition = windowRect.center.copy()
+var playerPosition = windowRect.center
 
-val points = createGrid(50, 40, 10.0)
+val points = createGrid(100, 80, 10.0)
+
+val dx = Vector2(2.0, 0.0)
+val dy = Vector2(0.0, 2.0)
 
 fun main() = application {
     configure {
@@ -38,60 +33,42 @@ fun main() = application {
             fps = 1.0 / deltaTime
             lastTime = currentTime
 
-            when (arrow){
-                ArrowKey.DOWN -> playerPosition = playerPosition.copy(y = playerPosition.y+2)
-                ArrowKey.UP -> playerPosition = playerPosition.copy(y = playerPosition.y-2)
-                ArrowKey.LEFT -> playerPosition = playerPosition.copy(x = playerPosition.x-2)
-                ArrowKey.RIGHT -> playerPosition = playerPosition.copy(x = playerPosition.x+2)
+            when (arrow) {
+                ArrowKey.DOWN -> playerPosition += dy
+                ArrowKey.UP -> playerPosition -= dy
+                ArrowKey.LEFT -> playerPosition -= dx
+                ArrowKey.RIGHT -> playerPosition += dx
                 null -> ""
             }
             drawer.fill = ColorRGBa.GREEN
             drawer.stroke = null
             drawer.circle(playerPosition, 10.0)
 
-            drawer.text(arrow?.name ?: "null", 10.0, 10.0)
-            drawer.text(playerPosition.toString(), 10.0, 30.0)
-            drawer.text(fps.toString(), 10.0, 60.0)
+            drawer.text(arrow?.name ?: "null", 10.0, 20.0)
+            drawer.text(playerPosition.toString(), 10.0, 40.0)
+            drawer.text(fps.toString().take(5), 10.0, 60.0)
 
             drawer.fill = ColorRGBa.WHITE
-            points.forEach{
-                drawer.point(it)
-            }
-
-
+            drawer.points(points)
         }
         keyboard.keyDown.listen {
-            when {
-                it.key == KEY_ARROW_UP -> arrow = ArrowKey.UP
-                it.key == KEY_ARROW_DOWN -> arrow = ArrowKey.DOWN
-                it.key == KEY_ARROW_LEFT -> arrow = ArrowKey.LEFT
-                it.key == KEY_ARROW_RIGHT -> arrow = ArrowKey.RIGHT
-
+            when (it.key) {
+                KEY_ARROW_UP -> arrow = ArrowKey.UP
+                KEY_ARROW_DOWN -> arrow = ArrowKey.DOWN
+                KEY_ARROW_LEFT -> arrow = ArrowKey.LEFT
+                KEY_ARROW_RIGHT -> arrow = ArrowKey.RIGHT
             }
         }
         keyboard.keyUp.listen {
-            when {
-                it.key == KEY_ARROW_UP -> if (arrow == ArrowKey.UP) arrow = null
-                it.key == KEY_ARROW_DOWN -> if (arrow == ArrowKey.DOWN) arrow = null
-                it.key == KEY_ARROW_LEFT -> if (arrow == ArrowKey.LEFT) arrow = null
-                it.key == KEY_ARROW_RIGHT -> if (arrow == ArrowKey.RIGHT) arrow = null
-
+            when (it.key) {
+                KEY_ARROW_UP -> if (arrow == ArrowKey.UP) arrow = null
+                KEY_ARROW_DOWN -> if (arrow == ArrowKey.DOWN) arrow = null
+                KEY_ARROW_LEFT -> if (arrow == ArrowKey.LEFT) arrow = null
+                KEY_ARROW_RIGHT -> if (arrow == ArrowKey.RIGHT) arrow = null
             }
         }
     }
 }
 
-fun createGrid(xSize: Int, ySize: Int, spacing: Double): List<Vector2> {
-    val points = mutableListOf<Vector2>()
-
-    for (i in 0 until xSize) {
-        for (j in 0 until ySize) {
-            val x = i * spacing
-            val y = j * spacing
-            val point = Vector2(x, y)
-            points.add(point)
-        }
-    }
-
-    return points
-}
+fun createGrid(xSize: Int, ySize: Int, spacing: Double) =
+    List(xSize) { x -> List(ySize) { y -> Vector2(x * spacing, y * spacing) } }.flatten()
